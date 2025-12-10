ddcb8ca6170294f4f0fddb96e9a1db5eb03a834f
ZenKD <okhello281@gmail.com>
2025-12-10 19:50:00 +0530

    Fix jsmediatags build error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // This tells Vite: "When code asks for 'jsmediatags', give it this specific file"
+      "jsmediatags": "jsmediatags/dist/jsmediatags.min.js",
+    },
+  },
 })
